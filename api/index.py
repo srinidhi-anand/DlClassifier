6a6fccab92b3d5fc723bc53529be43d5f7e99884
Srinidhi Anand <50217774+srinidhi-anand@users.noreply.github.com>
2025-05-06 13:05:38 +0530

    fix: basic temp changes

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 import uvicorn
 from api.schemas import FoodNote
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow as tf
+# import tensorflow as tf
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -33,98 +33,98 @@ app.add_middleware(
 )
 
 
-def load_img_predict(img_path):
-    img = tf.keras.preprocessing.image.load_img(img_path, target_size = (IMG_HEIGHT, IMG_WIDTH))
-    img = tf.keras.preprocessing.image.img_to_array(img)
-    img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
+# def load_img_predict(img_path):
+#     img = tf.keras.preprocessing.image.load_img(img_path, target_size = (IMG_HEIGHT, IMG_WIDTH))
+#     img = tf.keras.preprocessing.image.img_to_array(img)
+#     img = tf.keras.applications.mobilenet_v2.preprocess_input(img)
 
-    img = expand_dims(img, axis = 0)
-    print("image loaded for prediction!")
-    return img
+#     img = expand_dims(img, axis = 0)
+#     print("image loaded for prediction!")
+#     return img
 
-def predict_image(img_path,classifier):
-    print("image started for prediction!")
-    img = load_img_predict(img_path)
-    res = classifier.predict(img)
-    print(f"imagepredicted {res}")
-    classesList = []
-    with open('static/classes.json', 'r') as file:
-        classesList = json.load(file)
-    print(f"classesList {classesList}")
-    res = sorted (
-        list(zip ( 
-            classesList
-            , squeeze(res)
-         )
-        )
-     , key=lambda x: x[1]   
-     , reverse=True
-    )
-    print(f"res final {res}")
-    if (len(res) > 0):
-        return res[0]
-    return res
+# def predict_image(img_path,classifier):
+#     print("image started for prediction!")
+#     img = load_img_predict(img_path)
+#     res = classifier.predict(img)
+#     print(f"imagepredicted {res}")
+#     classesList = []
+#     with open('static/classes.json', 'r') as file:
+#         classesList = json.load(file)
+#     print(f"classesList {classesList}")
+#     res = sorted (
+#         list(zip ( 
+#             classesList
+#             , squeeze(res)
+#          )
+#         )
+#      , key=lambda x: x[1]   
+#      , reverse=True
+#     )
+#     print(f"res final {res}")
+#     if (len(res) > 0):
+#         return res[0]
+#     return res
 
-# def image_url_to_base64(image_url):
-#     """
-#     Converts an image URL to a base64 encoded string.
+# # def image_url_to_base64(image_url):
+# #     """
+# #     Converts an image URL to a base64 encoded string.
 
-#     Args:
-#         image_url: The URL of the image.
+# #     Args:
+# #         image_url: The URL of the image.
 
-#     Returns:
-#         A base64 encoded string of the image, or None if an error occurs.
-#     """
-#     try:
-#         response = requests.get(image_url, stream=True)
-#         response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+# #     Returns:
+# #         A base64 encoded string of the image, or None if an error occurs.
+# #     """
+# #     try:
+# #         response = requests.get(image_url, stream=True)
+# #         response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
  
-#         image = response.content
-#         base64_encoded = base64.b64encode(image).decode('utf-8')
-#         print(f" image fetched! {base64_encoded}")
-#         return base64_encoded
-#     except requests.exceptions.RequestException as e:
-#         print(f"Error fetching image: {e}")
-#         return None
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#         return None
+# #         image = response.content
+# #         base64_encoded = base64.b64encode(image).decode('utf-8')
+# #         print(f" image fetched! {base64_encoded}")
+# #         return base64_encoded
+# #     except requests.exceptions.RequestException as e:
+# #         print(f"Error fetching image: {e}")
+# #         return None
+# #     except Exception as e:
+# #         print(f"An error occurred: {e}")
+# #         return None
 
 
-# @app.post('/predict')
-@app.api_route("/predict", methods=["POST", "OPTIONS"])
-def main(body: Annotated[FoodNote, Form()]):
-    try:
-        contents = body.fileData.file.read()
-        # Process the file content
-        print(f"Uploaded file: {contents} {body.fileData.filename}")
-        model = tf.keras.models.load_model('static/model.h5')
-        print('from load saved!')
-        # print(model.summary())
-        # # tfjs.converters.save_keras_model(model, 'static/tfjs_model')
-        classifier = model # tf.keras.models.load_model('static/tfjs_model/model.json')
-        # img_data = image_url_to_base64(data.base64str)
-        img_path = f"static/{body.name}"
-        with open(img_path, "wb") as fh:
-            # fh.write(data.base64str.decode('base64'))
-            # fh.write(base64.urlsafe_b64decode(contents))
-            fh.write(contents)
-            fh.close()
-        result = predict_image(img_path,classifier)
-        print(f"predVal returned {result}")
-        label, predVal = result
-        predVal = round(predVal*100, 3)
-        print(f"result {label} {predVal}")
-        if os.path.exists(img_path):
-            os.remove(img_path)
-        else:
-            print(f"The file {img_path} does not exist")
-        if predVal >= 50:
-            return {'prediction': f"{str(predVal)}%", 'label': label }
-        else:
-            return {'prediction': f"{str(predVal)}%", 'label': 'unknown' }
-    except HTTPException as e:
-        raise HTTPException(status_code = e.status_code, detail=f'error occurred {e}')
+# # @app.post('/predict')
+# @app.api_route("/predict", methods=["POST", "OPTIONS"])
+# def main(body: Annotated[FoodNote, Form()]):
+#     try:
+#         contents = body.fileData.file.read()
+#         # Process the file content
+#         print(f"Uploaded file: {contents} {body.fileData.filename}")
+#         model = tf.keras.models.load_model('static/model.h5')
+#         print('from load saved!')
+#         # print(model.summary())
+#         # # tfjs.converters.save_keras_model(model, 'static/tfjs_model')
+#         classifier = model # tf.keras.models.load_model('static/tfjs_model/model.json')
+#         # img_data = image_url_to_base64(data.base64str)
+#         img_path = f"static/{body.name}"
+#         with open(img_path, "wb") as fh:
+#             # fh.write(data.base64str.decode('base64'))
+#             # fh.write(base64.urlsafe_b64decode(contents))
+#             fh.write(contents)
+#             fh.close()
+#         result = predict_image(img_path,classifier)
+#         print(f"predVal returned {result}")
+#         label, predVal = result
+#         predVal = round(predVal*100, 3)
+#         print(f"result {label} {predVal}")
+#         if os.path.exists(img_path):
+#             os.remove(img_path)
+#         else:
+#             print(f"The file {img_path} does not exist")
+#         if predVal >= 50:
+#             return {'prediction': f"{str(predVal)}%", 'label': label }
+#         else:
+#             return {'prediction': f"{str(predVal)}%", 'label': 'unknown' }
+#     except HTTPException as e:
+#         raise HTTPException(status_code = e.status_code, detail=f'error occurred {e}')
     
 @app.get("/")
 async def root():
