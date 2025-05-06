e0277c7d9b64b3e67232118967cbb2a87a216707
Srinidhi Anand <50217774+srinidhi-anand@users.noreply.github.com>
2025-05-06 19:49:17 +0530

    fix: removed python setup

--- api/schemas.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-from fastapi import Form, UploadFile
-from pydantic import BaseModel
-
-class FoodNote(BaseModel):
-    name: Annotated[str, Form()]
-    type: Annotated[str, Form()]
-    fileData: UploadFile
-
